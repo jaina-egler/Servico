b4c2a3ec830ac2b76744283c9972f23c154fbd86
jaina <jaina@alphasoftware.com.br>
2021-11-09 07:32:12 -0400

    Enviando pro Git

---
+++ MeuServico/FileIO.vb
@@ -0,0 +1,13 @@
+﻿Public Class FileIO
+    Public Shared Sub EscrevaNoArquivo(strEscrever As String)
+        Dim stream As IO.StreamWriter = Nothing 'Transfere
+        Try
+            stream = New IO.StreamWriter(AppDomain.CurrentDomain.BaseDirectory + "\arquivo.txt", True)
+            stream.Write(strEscrever) 'Escreve o texto passado
+            stream.Flush() 'Salva as mudanças
+            stream.Close() 'Fecha a stream
+        Catch ex As Exception
+
+        End Try
+    End Sub
+End Class
---
+++ MeuServico/MeuServico.vb
@@ -0,0 +1,24 @@
+﻿Public Class MeuServico
+
+
+    Dim tmp As Timers.Timer
+
+    Protected Overrides Sub OnStart(ByVal args() As String)
+        ' Adicione código aqui para iniciar seu serviço. Este método deve ajustar
+        ' o que é necessário para que seu serviço possa executar seu trabalho.
+        tmp = New Timers.Timer()
+        tmp.Interval = 1000 'Intervalo de tempo do timer, acontece a cada 1 segundo
+        AddHandler tmp.Elapsed, AddressOf Teste
+        tmp.Enabled = True
+        FileIO.EscrevaNoArquivo("Comecei... " + vbNewLine)
+    End Sub
+
+    Protected Overrides Sub OnStop()
+        ' Adicione código aqui para realizar qualquer limpeza necessária para parar seu serviço.
+        tmp.Enabled = False
+        FileIO.EscrevaNoArquivo("Parou " + vbNewLine)
+    End Sub
+    Private Sub Teste(obj As Object, e As EventArgs)
+        FileIO.EscrevaNoArquivo("Foi 1 segundo " + vbNewLine)
+    End Sub
+End Class
